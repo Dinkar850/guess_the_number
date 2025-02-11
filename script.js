d97dc74fe3477e1e48f8f7bb8d157e7cf51fdc6b
Dinkar850 <guptadinkar850@gmail.com>
2025-02-12 02:17:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let number = Math.trunc(Math.random() * 20) + 1;
+console.log(number);
+
+let score = 20;
+let highscore = 0;
+let lost = 0;
+const setMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+};
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        setMessage('No number!');
+    } else if (score === 0) {
+        setMessage('You lose!');
+        document.querySelector('body').style.backgroundColor =
+            'rgb(197, 6, 6) ';
+        lost = 1;
+    } else {
+        if (guess === number) {
+            document.querySelector('.number').textContent = number;
+            setMessage('Correct number!');
+            document.querySelector('body').style.backgroundColor = '#3f8928';
+            if (score > highscore) {
+                highscore = score;
+                document.querySelector('.highscore').textContent = score;
+            }
+
+            document.querySelector('.number').style.width = '30rem';
+        } else {
+            setMessage(guess > number ? 'Too high!' : 'Too low!');
+
+            score--;
+        }
+    }
+
+    document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+    if (!lost) number = Math.trunc(Math.random() * 20) + 1;
+    console.log(number);
+
+    score = 20;
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+});
